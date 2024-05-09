9321aac05f3467f329eb3763b033d7299da0338d
VincentiusKennedyW <kenken.kk40@gmail.com>
2024-05-09 18:51:53 +0800

    update response to only return id, judul, angkatan

--- app/Http/Resources/BookmarkResource.php
+++ app/Http/Resources/BookmarkResource.php
@@ -9,15 +9,9 @@ class BookmarkResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'skripsi' => [
-                'id' => $this->skripsi->id,
-                'judul' => $this->skripsi->judul,
-                'nama' => $this->skripsi->nama,
-                'nim' => $this->skripsi->nim,
-                'jurusan' => $this->skripsi->jurusan,
-                'angkatan' => $this->skripsi->angkatan,
-            ],
+            'id' => $this->skripsi->id,
+            'judul' => $this->skripsi->judul,
+            'angkatan' => $this->skripsi->angkatan,
             // tambahkan kolom lain dari model Bookmark jika perlu
         ];
     }
